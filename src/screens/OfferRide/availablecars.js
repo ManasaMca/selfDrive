ebd16c771cd5ba6667afc32123f13930a244dca6
sachin425 <sachin.bv@rapidsapi.com>
2021-02-20 11:52:04 +0530

    new changes post car

--- src/screens/OfferRide/availablecars.js
+++ src/screens/OfferRide/availablecars.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { View, Picker, StyleSheet, Text, TextInput, StatusBar, CheckBox, TouchableOpacity, Image, ScrollView,FlatList } from "react-native";
+import { View, Picker, StyleSheet, Text, TextInput, StatusBar, CheckBox, TouchableOpacity, Image, ScrollView, FlatList } from "react-native";
 //import * as Progress from 'react-native-progress';
 import BASE_URL from '../../constants/apiendpoints';
 import colors from '../../stylesheet/colors';
@@ -8,8 +8,8 @@ import { useRoute, useNavigation } from '@react-navigation/native';
 import styles from './availablecarsStyles';
 import Slider from '@react-native-community/slider';
 import Icon from 'react-native-vector-icons/FontAwesome5';
-import {carlist} from '../../Redux/selector/carselector';
-import {useSelector, useDispatch} from 'react-redux';
+import { carlist } from '../../Redux/selector/carselector';
+import { useSelector, useDispatch } from 'react-redux';
 
 
 const AvailableCars = () => {
@@ -45,44 +45,44 @@ const AvailableCars = () => {
                     />
                 </View>
                 <FlatList
-            data={cars_list}
-            renderItem={({ item }) => (
-                <View style={styles.background_view}>
-                    <View style={styles.view1}>
-                        <View style={styles.view2}>
-                            <View>
-                                <View style={styles.view3}>
-                                    <Image source={require('../../assets/DriverImg.png')} style={styles.image1} />
-                                    <Text style={styles.heading}>AR Travels</Text>
-                                </View>
-                                <Image source={{ uri: "http://udrive.b2bmart.org.in/api/" + item.capic }}
-                                        style={styles.car} />
-                            </View>
-                            <View>
-                                <Text style={styles.headtext2}>Rs: 2000/Day</Text>
-                                <View style={styles.vieww}>
-                                    <Text style={styles.txt}>Driver Provision</Text>
-                                    <Text style={styles.txt}>{item.carname}</Text>
-                                    <Text style={styles.txt}>{item.city}</Text>
-                                    <Text style={styles.txt}>{item.tdate}</Text>
-                                    
-                                    
+                    data={cars_list}
+                    renderItem={({ item }) => (
+                        <View style={styles.background_view}>
+                            <View style={styles.view1}>
+                                <View style={styles.view2}>
+                                    <View>
+                                        <View style={styles.view3}>
+                                            <Image source={require('../../assets/DriverImg.png')} style={styles.image1} />
+                                            <Text style={styles.heading}>AR Travels</Text>
+                                        </View>
+                                        <Image source={{ uri: "http://udrive.b2bmart.org.in/api/" + item.capic }}
+                                            style={styles.car} />
                                     </View>
+                                    <View>
+                                        <Text style={styles.headtext2}>Rs: 2000/Day</Text>
+                                        <View style={styles.vieww}>
+                                            <Text style={styles.txt}>Driver Provision</Text>
+                                            <Text style={styles.txt}>{item.carname}</Text>
+                                            <Text style={styles.txt}>{item.city}</Text>
+                                            <Text style={styles.txt}>{item.tdate}</Text>
 
-                                <TouchableOpacity style={styles.button2}
-                                    onPress={() => navigation.navigate('Cardetails')}
-                                >
-                                    <Text style={styles.button}>View Details</Text></TouchableOpacity>
+
+                                        </View>
+
+                                        <TouchableOpacity style={styles.button2}
+                                            onPress={() => navigation.navigate('Cardetails')}
+                                        >
+                                            <Text style={styles.button}>View Details</Text></TouchableOpacity>
+                                    </View>
+                                </View>
                             </View>
+
+
+
                         </View>
-                    </View>
-
-                  
-
-                </View>
-                  )}
-                  //Setting the number of column
-                  numColumns={1}
+                    )}
+                    //Setting the number of column
+                    numColumns={1}
                 //   keyExtractor={(item, index) => index.toString()}
                 />
 
